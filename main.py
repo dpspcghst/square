a70ba19d5aaf8133cef8c2f388631099e16f5026
Hemingway <ghostblac@protonmail.com>
2021-05-17 23:02:25 -0400

    Added the main.py file.

---
+++ main.py
@@ -0,0 +1,19 @@
+import math
+
+class Square():
+    """
+    """
+
+    def area_square(number: int):
+        """
+        """
+
+        area = number ** 2
+        print(f"Square | Solve for area: A = side ** 2 = {area}.")
+
+    def side_square(number: int):
+        """
+        """
+        
+        side = math.sqrt(number)
+        print(f"Square | Solve for side: a = square root of an area = {side}")
